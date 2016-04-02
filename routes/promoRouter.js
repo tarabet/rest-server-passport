0ff81ee723a083e5212a7ff969afdac9e7f53c22
Shuriken (ACER) <tarabet@yandex.ru>
2016-04-02 16:11:43 +0300

    initial commit

--- routes/promoRouter.js
+++ routes/promoRouter.js
@@ -1,59 +1,66 @@
-  var express = require('express');
-  var bodyParser = require('body-parser');
-  var mongoose = require('mongoose');
-  var Verify = require('./verify');
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var Verify    = require('./verify');
 
-  var Promotions = require('../models/promotions');
+var Promotions = require('../models/promotions');
+ 
+var promoRouter = express.Router();
 
-  var promoRouter = express.Router();
-  promoRouter.use(bodyParser.json());
+promoRouter.use(bodyParser.json());
 
-  promoRouter.route('/')
-    .get(Verify.verifyOrdinaryUser, function(req, res, next) {
-      Promotions.find({}, function(err, promo) {
-        if(err) throw err;
-        res.json(promo);
-      })
-    })
-    .post(Verify.verifyOrdinaryUser, Verify.verifyAdminUser, function(req, res, next){
-      Promotions.create(req.body, function(err, promo) {
-        if(err) throw err;
-        console.log('Promotion created');
-        var id = promo._id;
-        res.writeHead(200, { 'Content-Type' : 'text/plain' });
+promoRouter.route('/')
+.get(Verify.verifyOrdinaryUser, function (req, res, next) {
+    Promotions.find({}, function (err, promotion) {
+        if (err) throw err;
+        res.json(promotion);
+    });
+})
+
+.post(Verify.verifyOrdinaryUser, Verify.verifyAdmin, function (req, res, next) {
+    Promotions.create(req.body, function (err, promotion) {
+        if (err) throw err;
+        console.log('promotion created!');
+        var id = promotion._id;
+
+        res.writeHead(200, {
+            'Content-Type': 'text/plain'
+        });
         res.end('Added the promotion with id: ' + id);
-      });
-    })
-    .delete(Verify.verifyOrdinaryUser, Verify.verifyAdminUser, function(req, res, next){
-      Promotions.remove({}, function(err, resp) {
-        if(err) throw err;
-        res.json(resp);
-      })
     });
+})
 
-  promoRouter.route('/:promoId')
-    .get(Verify.verifyOrdinaryUser, function(req,res,next){
-      Promotions.findById(req.params.promoId, function(err, promo) {
-        if(err) throw err;
-        res.json(promo);
-      })
-    })
-    .put(Verify.verifyOrdinaryUser, Verify.verifyAdminUser, function(req, res, next){
-      Promotions.findByIdAndUpdate(req.params.promoId, {
-        $set: req.body
-      }, {
-        new: true
-      }, function(err, promo) {
-        if(err) throw err;
-        res.json(promo);
-      })
-    })
-    .delete(Verify.verifyOrdinaryUser, Verify.verifyAdminUser, function(req, res, next){
-      Promotions.findByIdAndRemove(req.params.promoId, function(err, resp) {
-        if(err) throw err;
+.delete(Verify.verifyOrdinaryUser, Verify.verifyAdmin, function (req, res, next) {
+    Promotions.remove({}, function (err, resp) {
+        if (err) throw err;
         res.json(resp);
-      })
     });
+});
+
+promoRouter.route('/:promotionId')
+.get(Verify.verifyOrdinaryUser, function (req, res, next) {
+    Promotions.findById(req.params.promotionId, function (err, promotion) {
+        if (err) throw err;
+        res.json(promotion);
+    });
+})
+
+.put(Verify.verifyOrdinaryUser, Verify.verifyAdmin, function (req, res, next) {
+    Promotions.findByIdAndUpdate(req.params.promotionId, {
+        $set: req.body
+    }, {
+        new: true
+    }, function (err, promotion) {
+        if (err) throw err;
+        res.json(promotion);
+    });
+})
+
+.delete(Verify.verifyOrdinaryUser, Verify.verifyAdmin, function (req, res, next) {
+    Promotions.findByIdAndRemove(req.params.promotionId, function (err, resp) {        if (err) throw err;
+        res.json(resp);
+    });
+});
 
 module.exports = promoRouter;
-
+ 
